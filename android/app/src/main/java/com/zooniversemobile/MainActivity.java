4c8a8534d6987f493b4a91a7b94522a12582501c
Robin Schaaf <robin@zooniverse.org>
2016-11-28 12:40:20 -0600

    Fix push notifications

--- android/app/src/main/java/com/zooniversemobile/MainActivity.java
+++ android/app/src/main/java/com/zooniversemobile/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends ReactActivity {
                 nativePusher.registerFCM(this, new PushNotificationRegistrationListener() {
                     @Override
                     public void onSuccessfulRegistration() {
-                        nativePusher.subscribe("general");
+                        nativePusher.subscribe("general2");
                     }
 
                     @Override
@@ -62,8 +62,12 @@ public class MainActivity extends ReactActivity {
                 @Override
                 public void onMessageReceived(RemoteMessage remoteMessage) {
                     //https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/RemoteMessage.Notification
-                    String title = remoteMessage.getNotification().getTitle();
-                    System.out.println("Title: " + title);
+                    System.out.println(">>>>received message fired!!");
+                    System.out.println(remoteMessage.getNotification());
+                    if (remoteMessage.getNotification() != null) {
+                        String title = remoteMessage.getNotification().getTitle();
+                        System.out.println("Title: " + title);
+                    }
 
                     //TODO: Use react native alert to display cross-platform alert here
 
